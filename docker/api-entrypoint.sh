d1045c7920b775dafa93f73f952f8fe614c4f2a5
“Gokul” <mamandurugokul1@gmail.com>
2026-05-14 07:25:48 +0530

    fixes in docker

--- docker/api-entrypoint.sh
+++ docker/api-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 if [ -z "${SKIP_MIGRATE}" ]; then
-  prisma migrate deploy --schema=/app/packages/db/prisma/schema.prisma
+  /app/node_modules/.bin/prisma migrate deploy --schema=/app/packages/db/prisma/schema.prisma
 fi
 exec node /app/apps/api/dist/index.js
